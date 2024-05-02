1cd066af74ae4736cc5cbedf27db0e286d836c17
klever-talha-projects <97839278+klever-talha-projects@users.noreply.github.com>
2024-05-03 02:35:36 +0500

    Update script.js

--- klever-crypto-tools/script.js
+++ klever-crypto-tools/script.js
@@ -29,33 +29,3 @@ const searchInput = document.querySelector(".search__wrapper input");
 searchInput.addEventListener("focus", (e) => {
   document.body.classList.remove("collapsed");
 });
-
-
-let navItems = document.querySelectorAll(".nav-item")
-let dropDownItems = document.querySelectorAll(".drop-down-items")
-
-if (window.innerWidth < window.innerHeight * 2) {
-  // Hide all dropdown items
-  dropDownItems.forEach(function (item) {
-    item.style.display = "none";
-  });
-
-  // Add click event listeners to navItems
-  navItems.forEach(function (navItem, index) {
-    navItem.addEventListener("click", () => {
-      // Hide all dropdown items except the one clicked
-      dropDownItems.forEach(function (item, i) {
-        if (i !== index) {
-          item.style.display = "none";
-        }
-      });
-
-      // Toggle display of the clicked dropdown item
-      if (dropDownItems[index].style.display !== "none") {
-        dropDownItems[index].style.display = "none";
-      } else {
-        dropDownItems[index].style.display = "flex";
-      }
-    });
-  });
-}
